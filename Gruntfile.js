f74d92a0922de7c5587f54a38b2de65590975ef0
Nick Freear <nfreear@yahoo.co.uk>
2016-03-04 17:13:28 +0000

    Bug #26, Add `grunt-nice-package` validator to `package` JSON and Gruntfile
    * [iet:5243437]

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,8 @@ module.exports = function(grunt) {
 					// I18n / translation - list available languages, except 'en' [Bug: #3]
 					'locales': [
 						'el',
-						'es'
+						'es',
+						'fr'
 					],
 					poFilesPath: 'po/<%= locale %>.po'
 				}
@@ -65,6 +66,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-angular-gettext');
 	grunt.loadNpmTasks('grunt-msg-init-merge');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-nice-package');
 	grunt.loadNpmTasks('grunt-sass');
 
 	grunt.registerTask('gettext', [
@@ -73,5 +75,5 @@ module.exports = function(grunt) {
 		'msgInitMerge'
 	]);
 
-	grunt.registerTask('default', [ 'gettext', 'sass', 'jshint:config' ]);
+	grunt.registerTask('default', [ 'gettext', 'sass', 'jshint:config', 'nice-package' ]);
 };
