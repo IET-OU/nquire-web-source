a63aa616eec3571c8f3b2bbd025f7d272dff0753
Nick Freear <nfreear@yahoo.co.uk>
2016-03-18 16:26:53 +0000

    Bug #28, Add `replace` Angular.JS javascript filter [iet:4777415]

---
+++ static/src/js/app/filters/replace.js
@@ -0,0 +1,19 @@
+/*!
+  Angular filter to replace text.
+
+  Created by Nick Freear, 16 March 2016.
+*/
+
+angular.module('senseItWeb', null, null).filter('replace', function () {
+    'use strict';
+
+    return function (input, pattern, newSubStr, flags) {
+        var regex = pattern ? new RegExp(pattern, flags) : null;
+	      //Was:
+        console.log("Filter replace: ", input, pattern, regex, newSubStr, flags);
+	      if (input && regex && typeof newSubStr === 'string') {
+	          return input.replace(regex, newSubStr);
+	      }
+	      return input;
+    };
+});
