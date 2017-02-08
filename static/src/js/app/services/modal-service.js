3dd7e1da16b389a47bd35a364788855a7c4960e0
Nick Freear <nfreear@yahoo.co.uk>
2017-02-08 15:04:57 +0000

    Bug #45, fix for `modal-service` JS - `$uibModal` [iet:7900539][iet:7900963]

--- static/src/js/app/services/modal-service.js
+++ static/src/js/app/services/modal-service.js
@@ -1,10 +1,12 @@
 
-angular.module('senseItServices', null, null).factory('ModalService', ['$modal', function ($modal) {
+angular.module('senseItServices', null, null).factory('ModalService', ['$uibModal', function ($uibModal) {
+    // Was: '$modal'
+
     'use strict';
 
     return {
         open: function (data) {
-            $modal.open({
+            $uibModal.open({
                 templateUrl: 'partials/widgets/modal-template.html',
                 controller: 'ModalCtrl',
                 size: data.size || 'sm',
