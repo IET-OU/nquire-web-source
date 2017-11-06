aece836a2ef7ad2c4e3be47712fcfe1859cbfb44
Nick Freear <nfreear@yahoo.co.uk>
2017-11-06 15:00:55 +0000

    Pull #53, follow-up to "fixes sidebar: selected items" [iet:10264031]
    * (Don't call `console.log()` JS function directly.)

--- static/src/js/app/controllers/project-list/project-filter-sidebar-controller.js
+++ static/src/js/app/controllers/project-list/project-filter-sidebar-controller.js
@@ -4,7 +4,7 @@
 angular.module('senseItWeb', null, null).controller('ProjectFilterSidebarCtrl', function ($scope, $state) {
 
     $scope.menuItemClass = function(key, value) {
-        console.log(key, value, $state.params[key]);
-        return ($state.params[key] || null) === value ? 'selected' : '';
+        $scope.log(key, value, $state.params[ key ]);  // console.log(key, value, $state.params[key]);
+        return ($state.params[ key ] || null) === value ? 'selected' : '';
     };
 });
