72a835a1af8358bb583adea96946d470ac5f935b
Nick Freear <nfreear@yahoo.co.uk>
2018-02-20 16:20:26 +0000

    Fix protocol `youtube-video` Angular JS filter [iet:10307376]

--- static/src/js/app/filters/youtube-video.js
+++ static/src/js/app/filters/youtube-video.js
@@ -2,6 +2,6 @@ angular.module('senseItWeb', null, null).filter('youtubeVideo', ['$sce', functio
   return function (value) {
     // Parse for a video ID.
 	var m = value.match(/v=([^&]+)/);
-    return $sce.trustAsResourceUrl('//www.youtube.com/embed/' + (m ? m[1] : value));
+    return $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + (m ? m[ 1 ] : value));
   };
 }]);
