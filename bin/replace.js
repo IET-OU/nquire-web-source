0de2713048975e6b2de98dc69f515df3c267addc
Nick Freear <nfreear@yahoo.co.uk>
2018-09-07 16:03:39 +0100

    Bug #62, Integrate `bin/git-version` and `bin/replace` directly in Gruntfile.JS
    * https://iet.eu.teamwork.com/desk/#/tickets/264317

--- bin/replace.js
+++ bin/replace.js
@@ -1,11 +1,11 @@
-#!/usr/bin/env node
-
 /*!
   Update a "random" parameter for all <script> and CSS includes in "index.html". Plus, add version.
 
   @copyright Nick Freear, 15-November-2017.
   @link  http://www.nquire-it.org/version.json
 */
+
+module.exports = replace_config_js_html;
 
 const replace = require('replace');
 const version = require('./../static/src/version.json');
@@ -16,47 +16,52 @@ const RAND = getRandomInt(11, 1000);
 const PKG = require('./../package.json');
 const ENV = require('./../.env.json');
 
-console.warn('Version.json ~ describe, rand:', version.describe, RAND);
+function replace_config_js_html (grunt) {
 
-replace({
-  paths: [ INDEX_HTML ],
-  regex: /\.(css|js)\?r=(_RAND_|[^\"]+)/g,
-  replacement: '.$1?r=' + RAND,
-  count: true,
-  recursive: false
-});
+  grunt.log.ok('Version.json ~ describe, rand:', version.describe, RAND);
 
-replace({
-  paths: [ INDEX_HTML ],
-  regex: /content="nQuire-it\/(_VERSION_|[^\"]+)"/,
-  replacement: 'content="nQuire-it/%s"'.replace(/%s/, version.describe),
-  count: true,
-  recursive: false
-});
+  // console.warn('Version.json ~ describe, rand:', version.describe, RAND);
 
-replace({
-  paths: [ INDEX_HTML ],
-  regex: /\/maps\/api\/js\?key=(_GOOGLE_MAP_KEY_|[\w_\-]+)/,
-  replacement: '/maps/api/js?key=%s'.replace(/%s/, ENV._GOOGLE_MAP_KEY_),
-  count: true,
-  recursive: false
-});
+  replace({
+    paths: [ INDEX_HTML ],
+    regex: /\.(css|js)\?r=(_RAND_|[^\"]+)/g,
+    replacement: '.$1?r=' + RAND,
+    count: true,
+    recursive: false
+  });
 
-replace({
-  paths: [ CONFIG_JS ],
-  regex: /version: ['"][\w_\.\-]+['"],/,
-  replacement: 'version: "%s",'.replace(/%s/, version.describe),
-  count: true,
-  recursive: false
-});
+  replace({
+    paths: [ INDEX_HTML ],
+    regex: /content="nQuire-it\/(_VERSION_|[^\"]+)"/,
+    replacement: 'content="nQuire-it/%s"'.replace(/%s/, version.describe),
+    count: true,
+    recursive: false
+  });
 
-replace({
-  paths: [ CONFIG_JS ],
-  regex: /build_time: ['"][\w_\.\-:]+['"],/,
-  replacement: 'build_time: "%s",'.replace(/%s/, new Date().toISOString()),
-  count: true,
-  recursive: false
-});
+  replace({
+    paths: [ INDEX_HTML ],
+    regex: /\/maps\/api\/js\?key=(_GOOGLE_MAP_KEY_|[\w_\-]+)/,
+    replacement: '/maps/api/js?key=%s'.replace(/%s/, ENV._GOOGLE_MAP_KEY_),
+    count: true,
+    recursive: false
+  });
+
+  replace({
+    paths: [ CONFIG_JS ],
+    regex: /version: ['"][\w_\.\-]+['"],/,
+    replacement: 'version: "%s",'.replace(/%s/, version.describe),
+    count: true,
+    recursive: false
+  });
+
+  replace({
+    paths: [ CONFIG_JS ],
+    regex: /build_time: ['"][\w_\.\-:]+['"],/,
+    replacement: 'build_time: "%s",'.replace(/%s/, new Date().toISOString()),
+    count: true,
+    recursive: false
+  });
+}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
